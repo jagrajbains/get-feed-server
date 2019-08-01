f232d7519a22fad08db56d66e299a446c6167663
Jagraj Bains <jagrajbains.94@gmail.com>
2019-08-01 10:55:01 +0530

    Added Billing and Client side app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Get-Feed!</h1>
+      <p>Collect your feedback.</p>
+    </div>
+  );
+};
+
+export default Landing;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
 require("./models/User");
 require("./services/passport");
 const mongoose = require("mongoose");
@@ -10,6 +11,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -21,6 +23,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  //Express will serve up our production files
+  // Like main.js or main.css files
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
